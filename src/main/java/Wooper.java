469c9ef6b22d9fa8f72c6c2f963a00ded93ef597
rxlee04 <rxlee04@gmail.com>
2026-01-23 02:09:33 +0800

    wooper echo

--- src/main/java/Wooper.java
+++ src/main/java/Wooper.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Wooper {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         String divider = "____________________________________________________________";
-        String msg = divider + "\n Hello! I'm Wooper\n" +
-                " What can I do for you? \n" + divider + "\n Bye. Hope to see you again soon!\n" + divider;
+        String greetMsg = divider + "\n Woo-pah! I'm Wooper\n" +
+                " What can I do for you? \n" + divider;
 
-        System.out.println(msg);
+        String exitMsg = divider + "\n Woop Woop!. Hope to see you again soon!\n" + divider;
+        System.out.println(greetMsg);
 
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println(divider);
+            System.out.println(userInput);
+            System.out.println(divider);
+            userInput = scanner.nextLine();
+        }
+        System.out.println(exitMsg);
     }
 }
