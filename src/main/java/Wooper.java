6316b0aebfd4a1036190924b68883c523ccc10a5
rxlee04 <rxlee04@gmail.com>
2026-01-23 01:55:05 +0800

    New Wooper Chatbot

---
+++ src/main/java/Wooper.java
@@ -0,0 +1,10 @@
+public class Wooper {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        String msg = divider + "\n Hello! I'm Wooper\n" +
+                " What can I do for you? \n" + divider + "\n Bye. Hope to see you again soon!\n" + divider;
+
+        System.out.println(msg);
+
+    }
+}
