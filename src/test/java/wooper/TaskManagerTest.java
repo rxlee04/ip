c38adf32d0c76b7d9ae84cef5eb4860f3abed801
rxlee04 <rxlee04@gmail.com>
2026-02-18 01:54:49 +0800

    Add more unit testing

--- src/test/java/wooper/TaskManagerTest.java
+++ src/test/java/wooper/TaskManagerTest.java
@@ -5,11 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.temporal.Temporal;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import wooper.exception.WooperException;
 import wooper.task.Task;
+import wooper.task.ToDo;
 
 public class TaskManagerTest {
     private TaskManager tm;
@@ -19,22 +26,42 @@ public class TaskManagerTest {
         tm = new TaskManager();
     }
 
+    /**
+     * getAllTasks / getTaskListSize
+     */
     @Test
-    public void markTaskDoneTest_validIndex_markDone() throws WooperException {
+    public void getTaskListSize_emptyTaskList_zeroReturned() {
+        assertEquals(0, tm.getTaskListSize());
+    }
+
+    @Test
+    public void getAllTasks_afterAddingTask_listWithOneTaskReturned() throws WooperException {
+        tm.addToDoTask("read");
+        assertEquals(1, tm.getAllTasks().size());
+    }
+
+    /**
+     * markTaskDone
+     */
+    @Test
+    public void markTaskDone_validIndex_taskMarkedDone() throws WooperException {
         tm.addToDoTask("read book");
         Task t = tm.markTaskDone(0);
         assertTrue(t.isDone());
     }
 
     @Test
-    public void markTaskDoneTest_invalidIndex_markDoneFail() throws WooperException {
+    public void markTaskDone_invalidIndex_exceptionThrown() throws WooperException {
         tm.addToDoTask("read book");
         assertThrows(WooperException.class, () -> tm.markTaskDone(99));
         assertThrows(WooperException.class, () -> tm.markTaskDone(-1));
     }
 
+    /**
+     * unmarkTaskDone
+     */
     @Test
-    public void unmarkTaskDoneTest_validIndex_markUndone() throws WooperException {
+    public void unmarkTaskDone_validIndex_taskMarkedUndone() throws WooperException {
         tm.addToDoTask("read book");
         Task t = tm.markTaskDone(0);
         tm.unmarkTaskDone(0);
@@ -42,24 +69,288 @@ public class TaskManagerTest {
     }
 
     @Test
-    public void unmarkTaskDoneTest_invalidIndex_markUndoneFail() throws WooperException {
+    public void unmarkTaskDone_invalidIndex_exceptionThrown() throws WooperException {
         tm.addToDoTask("read book");
         tm.markTaskDone(0);
         assertThrows(WooperException.class, () -> tm.unmarkTaskDone(99));
         assertThrows(WooperException.class, () -> tm.unmarkTaskDone(-1));
     }
 
+    /**
+     * addToDoTask
+     */
     @Test
-    public void deleteTaskTest_validIndex_taskDeleted() throws WooperException {
+    public void addToDoTask_emptyDescription_exceptionThrown() {
+        assertThrows(WooperException.class, () -> tm.addToDoTask(""));
+    }
+
+    @Test
+    public void addToDoTask_blankDescription_exceptionThrown() {
+        assertThrows(WooperException.class, () -> tm.addToDoTask("   "));
+    }
+
+    @Test
+    public void addToDoTask_validDescription_taskAdded() throws WooperException {
+        tm.addToDoTask("read book");
+        assertEquals(1, tm.getTaskListSize());
+        assertEquals("read book", tm.getAllTasks().get(0).getTaskName());
+    }
+
+    /**
+     * addDeadlineTask
+     */
+    @Test
+    public void addDeadlineTask_localDate_taskAdded() throws WooperException {
+        tm.addDeadlineTask("return book", LocalDate.of(2026, 6, 6));
+        assertEquals(1, tm.getTaskListSize());
+    }
+
+    @Test
+    public void addDeadlineTask_localDateTime_taskAdded() throws WooperException {
+        tm.addDeadlineTask("submit",
+                LocalDateTime.of(2026, 6, 6, 14, 0));
+        assertEquals(1, tm.getTaskListSize());
+    }
+
+    @Test
+    public void addDeadlineTask_blankDescription_exceptionThrown() {
+        assertThrows(WooperException.class, () -> tm.addDeadlineTask("   ",
+                LocalDate.of(2026, 6, 6)));
+    }
+
+    @Test
+    public void addDeadlineTask_invalidDate_exceptionThrown() {
+        Temporal weird = Year.of(2026);
+        assertThrows(WooperException.class, () -> tm.addDeadlineTask("x", weird));
+    }
+
+    /**
+     * addEventTask
+     */
+    @Test
+    public void addEventTask_localDate_taskAdded() throws WooperException {
+        tm.addEventTask("camp",
+                LocalDate.of(2026, 2, 2),
+                LocalDate.of(2026, 2, 3));
+        assertEquals(1, tm.getTaskListSize());
+    }
+
+    @Test
+    public void addEventTask_localDateTime_taskAdded() throws WooperException {
+        tm.addEventTask("meeting",
+                LocalDateTime.of(2026, 2, 2, 14, 0),
+                LocalDateTime.of(2026, 2, 2, 16, 0));
+        assertEquals(1, tm.getTaskListSize());
+    }
+
+    @Test
+    public void addEventTask_mixedTemporalTypes_exceptionThrown() {
+        assertThrows(WooperException.class, () -> tm.addEventTask("meeting",
+                LocalDate.of(2026, 2, 2),
+                LocalDateTime.of(2026, 2, 2, 16, 0)));
+    }
+
+    @Test
+    public void addEventTask_blankDescription_exceptionThrown() {
+        assertThrows(WooperException.class, () -> tm.addEventTask("   ",
+                LocalDate.of(2026, 2, 2),
+                LocalDate.of(2026, 2, 3)));
+    }
+
+    /**
+     * deleteTask
+     */
+    @Test
+    public void deleteTask_validIndex_taskRemoved() throws WooperException {
         tm.addToDoTask("read book");
         tm.deleteTask(0);
         assertEquals(0, tm.getTaskListSize());
     }
 
     @Test
-    public void deleteTaskTest_invalidIndex_taskNotDeleted() throws WooperException {
+    public void deleteTask_invalidIndex_exceptionThrown() throws WooperException {
         tm.addToDoTask("read book");
         assertThrows(WooperException.class, () -> tm.deleteTask(99));
         assertThrows(WooperException.class, () -> tm.deleteTask(-1));
+    }
+
+    /**
+     * loadTaskList
+     */
+    @Test
+    public void loadTaskList_validList_tasksLoaded() {
+        ArrayList<Task> list = new ArrayList<>();
+        list.add(new ToDo("a"));
+        list.add(new ToDo("b"));
+
+        tm.loadTaskList(list);
+        assertEquals(2, tm.getTaskListSize());
+    }
+
+    /**
+     * findTasks
+     */
+    @Test
+    public void findTasks_caseInsensitiveKeyword_matchingTasksReturned() throws WooperException {
+        tm.addToDoTask("Read Book");
+        assertEquals(1, tm.findTasks("read").size());
+        assertEquals(1, tm.findTasks("BOOK").size());
+    }
+
+    @Test
+    public void findTasks_blankKeyword_exceptionThrown() throws WooperException {
+        tm.addToDoTask("read book");
+        assertThrows(WooperException.class, () -> tm.findTasks("   "));
+    }
+
+    @Test
+    public void findTasks_noMatch_emptyListReturned() throws WooperException {
+        tm.addToDoTask("read book");
+        assertEquals(0, tm.findTasks("gym").size());
+    }
+
+    /**
+     * updateTask
+     */
+    @Test
+    public void updateTask_invalidIndex_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> args = buildUpdateArgs("todo", "new", "", "", "");
+        assertThrows(WooperException.class, () -> tm.updateTask(99, args));
+        assertThrows(WooperException.class, () -> tm.updateTask(-1, args));
+    }
+
+    @Test
+    public void updateTask_nullArgs_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, null));
+    }
+
+    @Test
+    public void updateTask_argsTooShort_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> bad = new ArrayList<>();
+        bad.add("update"); // size < 6
+        assertThrows(WooperException.class, () -> tm.updateTask(0, bad));
+    }
+
+    @Test
+    public void updateTask_emptyTaskType_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> args = buildUpdateArgs("   ", "new", "", "", "");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_wrongTaskType_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old"); // actual type is todo
+        ArrayList<String> args = buildUpdateArgs("deadline", "new", "", "", "");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_noFieldsProvided_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> args = buildUpdateArgs("todo", "   ", "   ", "   ", "   ");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_todoTaskNameProvided_taskNameUpdated() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> args = buildUpdateArgs("todo", "new name", "", "", "");
+
+        Task updated = tm.updateTask(0, args);
+        assertEquals("new name", updated.getTaskName());
+    }
+
+    @Test
+    public void updateTask_todoByProvided_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> args = buildUpdateArgs("todo", "", "06/06/2026", "", "");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_todoFromToProvided_exceptionThrown() throws WooperException {
+        tm.addToDoTask("old");
+        ArrayList<String> args = buildUpdateArgs("todo", "", "", "02/02/2026", "03/02/2026");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_deadlineByProvided_deadlineUpdated() throws WooperException {
+        tm.addDeadlineTask("dl", LocalDate.of(2026, 6, 6));
+
+        tm.updateTask(0, buildUpdateArgs("deadline", "", "07/06/2026", "", ""));
+
+        wooper.task.Deadline d = (wooper.task.Deadline) tm.getAllTasks().get(0);
+        assertEquals("2026-06-07", d.getDeadlineDueBy());
+    }
+
+    @Test
+    public void updateTask_deadlineFromToProvided_exceptionThrown() throws WooperException {
+        tm.addDeadlineTask("dl", LocalDate.of(2026, 6, 6));
+        ArrayList<String> args = buildUpdateArgs("deadline", "", "", "02/02/2026", "03/02/2026");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_eventFromToProvided_eventUpdated() throws WooperException {
+        tm.addEventTask("event",
+                LocalDateTime.of(2026, 2, 2, 14, 0),
+                LocalDateTime.of(2026, 2, 2, 16, 0));
+
+        ArrayList<String> args = buildUpdateArgs("event", "", "", "03/02/2026 10:00", "03/02/2026 11:00");
+        tm.updateTask(0, args);
+
+        Task t = tm.getAllTasks().get(0);
+        assertTrue(t instanceof wooper.task.Event);
+
+        wooper.task.Event e = (wooper.task.Event) t;
+        assertEquals(LocalDateTime.of(2026, 2, 3, 10, 0), LocalDateTime.parse(e.getEventStart()));
+        assertEquals(LocalDateTime.of(2026, 2, 3, 11, 0), LocalDateTime.parse(e.getEventEnd()));
+    }
+
+    @Test
+    public void updateTask_eventByProvided_exceptionThrown() throws WooperException {
+        tm.addEventTask("event",
+                LocalDateTime.of(2026, 2, 2, 14, 0),
+                LocalDateTime.of(2026, 2, 2, 16, 0));
+        ArrayList<String> args = buildUpdateArgs("event", "", "06/06/2026", "", "");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_eventOnlyFromProvided_exceptionThrown() throws WooperException {
+        tm.addEventTask("event",
+                LocalDateTime.of(2026, 2, 2, 14, 0),
+                LocalDateTime.of(2026, 2, 2, 16, 0));
+        ArrayList<String> args = buildUpdateArgs("event", "", "", "02/02/2026 14:00", "");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    @Test
+    public void updateTask_eventOnlyToProvided_exceptionThrown() throws WooperException {
+        tm.addEventTask("event",
+                LocalDateTime.of(2026, 2, 2, 14, 0),
+                LocalDateTime.of(2026, 2, 2, 16, 0));
+        ArrayList<String> args = buildUpdateArgs("event", "", "", "", "02/02/2026 16:00");
+        assertThrows(WooperException.class, () -> tm.updateTask(0, args));
+    }
+
+    /**
+     * Helper: builds args list in the exact positions TaskManager expects.
+     * args.get(1)=taskType, get(2)=taskName, get(3)=by, get(4)=from, get(5)=to
+     */
+    private ArrayList<String> buildUpdateArgs(String taskType, String taskName, String by, String from, String to) {
+        ArrayList<String> args = new ArrayList<>();
+        args.add("update");
+        args.add(taskType);
+        args.add(taskName);
+        args.add(by);
+        args.add(from);
+        args.add(to);
+        return args;
     }
 }
--- src/test/java/wooper/parser/ParserTest.java
+++ src/test/java/wooper/parser/ParserTest.java
@@ -10,81 +10,229 @@ import org.junit.jupiter.api.Test;
 import wooper.enums.CommandType;
 
 public class ParserTest {
-    private Parser parser = new Parser();
+    private final Parser parser = new Parser();
 
+    /**
+     * getActionAndArguments
+     */
     @Test
-    public void getActionAndArguments_validListInput_returnsListCommand() {
-        ParseResult expected = new ParseResult(CommandType.LIST);
+    public void getActionAndArguments_listCommand_listReturned() {
         ParseResult actual = parser.getActionAndArguments("list");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
+        assertEquals(CommandType.LIST, actual.getCommandType());
     }
 
     @Test
-    public void getActionAndArguments_validMarkInput_returnsMarkCommand() {
-        ArrayList<String> args = new ArrayList<>(List.of("1"));
-        ParseResult expected = new ParseResult(CommandType.MARK, args);
+    public void getActionAndArguments_markCommandWithIndex_markReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("1"));
         ParseResult actual = parser.getActionAndArguments("mark 1");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
-        assertEquals(args, actual.getArgs());
+        assertEquals(CommandType.MARK, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 
     @Test
-    public void getActionAndArguments_validUnmarkInput_returnsUnmarkCommand() {
-        ArrayList<String> args = new ArrayList<>(List.of("1"));
-        ParseResult expected = new ParseResult(CommandType.UNMARK, args);
+    public void getActionAndArguments_unmarkCommandWithIndex_unmarkReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("1"));
         ParseResult actual = parser.getActionAndArguments("unmark 1");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
-        assertEquals(args, actual.getArgs());
+        assertEquals(CommandType.UNMARK, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 
     @Test
-    public void getActionAndArguments_validTodoInput_returnsTodoCommand() {
-        ArrayList<String> args = new ArrayList<>(List.of("taskName"));
-        ParseResult expected = new ParseResult(CommandType.TODO, args);
+    public void getActionAndArguments_todoWithTaskName_todoReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("taskName"));
         ParseResult actual = parser.getActionAndArguments("todo taskName");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
-        assertEquals(args, actual.getArgs());
+        assertEquals(CommandType.TODO, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 
     @Test
-    public void getActionAndArguments_validDeadlineInput_returnsDeadlineCommand() {
-        ArrayList<String> args = new ArrayList<>(List.of("return book", "06/06/2026"));
-        ParseResult expected = new ParseResult(CommandType.DEADLINE, args);
+    public void getActionAndArguments_deadlineWithBy_deadlineReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("return book", "06/06/2026"));
         ParseResult actual = parser.getActionAndArguments("deadline return book /by 06/06/2026");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
-        assertEquals(args, actual.getArgs());
+        assertEquals(CommandType.DEADLINE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 
     @Test
-    public void getActionAndArguments_validEventInput_returnsEventCommand() {
-        ArrayList<String> args = new ArrayList<>(List.of("project meeting", "06/08/2026 14:00", "06/08/2026 16:00"));
-        ParseResult expected = new ParseResult(CommandType.EVENT, args);
+    public void getActionAndArguments_eventWithFromTo_eventReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(
+                List.of("project meeting", "06/08/2026 14:00", "06/08/2026 16:00"));
         ParseResult actual = parser
                 .getActionAndArguments("event project meeting /from 06/08/2026 14:00 /to 06/08/2026 16:00");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
-        assertEquals(args, actual.getArgs());
+        assertEquals(CommandType.EVENT, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 
     @Test
-    public void getActionAndArguments_validDeleteInput_returnsDeleteCommand() {
-        ArrayList<String> args = new ArrayList<>(List.of("1"));
-        ParseResult expected = new ParseResult(CommandType.DELETE, args);
+    public void getActionAndArguments_deleteWithIndex_deleteReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("1"));
         ParseResult actual = parser.getActionAndArguments("delete 1");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
-        assertEquals(args, actual.getArgs());
+        assertEquals(CommandType.DELETE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 
     @Test
-    public void getActionAndArguments_validByeInput_returnsByeCommand() {
-        ParseResult expected = new ParseResult(CommandType.BYE);
+    public void getActionAndArguments_byeCommand_byeReturned() {
         ParseResult actual = parser.getActionAndArguments("bye");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
+        assertEquals(CommandType.BYE, actual.getCommandType());
     }
 
     @Test
-    public void getActionAndArguments_invalidInput_returnsUnknownCommand() {
-        ParseResult expected = new ParseResult(CommandType.UNKNOWN);
+    public void getActionAndArguments_unknownCommand_unknownReturned() {
         ParseResult actual = parser.getActionAndArguments("anything");
-        assertEquals(expected.getCommandType(), actual.getCommandType());
+        assertEquals(CommandType.UNKNOWN, actual.getCommandType());
+    }
+
+    /**
+     * getActionAndArguments edge cases
+     */
+    @Test
+    public void getActionAndArguments_nullInput_unknownReturned() {
+        ParseResult actual = parser.getActionAndArguments(null);
+        assertEquals(CommandType.UNKNOWN, actual.getCommandType());
+        assertEquals(new ArrayList<>(), actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_blankInput_unknownReturned() {
+        ParseResult actual = parser.getActionAndArguments("   ");
+        assertEquals(CommandType.UNKNOWN, actual.getCommandType());
+        assertEquals(new ArrayList<>(), actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_commandWithLeadingTrailingSpaces_parsedCorrectly() {
+        ParseResult actual = parser.getActionAndArguments("   list   ");
+        assertEquals(CommandType.LIST, actual.getCommandType());
+    }
+
+    @Test
+    public void getActionAndArguments_markWithoutArg_markReturnedWithEmptyArg() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of(""));
+        ParseResult actual = parser.getActionAndArguments("mark");
+        assertEquals(CommandType.MARK, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_deleteWithoutArg_deleteReturnedWithEmptyArg() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of(""));
+        ParseResult actual = parser.getActionAndArguments("delete");
+        assertEquals(CommandType.DELETE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_findCommandWithKeyword_findReturned() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("book"));
+        ParseResult actual = parser.getActionAndArguments("find book");
+        assertEquals(CommandType.FIND, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    /**
+     * deadline parsing edge cases
+     */
+    @Test
+    public void getActionAndArguments_deadlineWithoutBy_deadlineReturnedWithEmptyBy() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("return book", ""));
+        ParseResult actual = parser.getActionAndArguments("deadline return book");
+        assertEquals(CommandType.DEADLINE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_deadlineByButNoDate_deadlineReturnedWithEmptyByValue() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("return book", ""));
+        ParseResult actual = parser.getActionAndArguments("deadline return book /by");
+        assertEquals(CommandType.DEADLINE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    /**
+     * event parsing edge cases
+     */
+    @Test
+    public void getActionAndArguments_eventMissingFrom_eventReturnedWithEmptyFromTo() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("meeting /to 06/08/2026 16:00", "", ""));
+        ParseResult actual = parser.getActionAndArguments("event meeting /to 06/08/2026 16:00");
+        assertEquals(CommandType.EVENT, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_eventMissingTo_eventReturnedWithEmptyFromTo() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("meeting /from 06/08/2026 14:00", "", ""));
+        ParseResult actual = parser.getActionAndArguments("event meeting /from 06/08/2026 14:00");
+        assertEquals(CommandType.EVENT, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_eventWrongOrderToBeforeFrom_eventReturnedWithEmptyFromTo() {
+        ArrayList<String> expectedArgs = new ArrayList<>(
+                List.of("meeting /to 06/08/2026 16:00 /from 06/08/2026 14:00", "", ""));
+        ParseResult actual = parser
+                .getActionAndArguments("event meeting /to 06/08/2026 16:00 /from 06/08/2026 14:00");
+        assertEquals(CommandType.EVENT, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    /**
+     * update parsing
+     *
+     * Parser returns args in this order:
+     * [0]=taskNo, [1]=taskType, [2]=taskName, [3]=by, [4]=from, [5]=to
+     */
+    @Test
+    public void getActionAndArguments_updateAllFieldsProvided_updateArgsParsed() {
+        ArrayList<String> expectedArgs = new ArrayList<>(
+                List.of("1", "todo", "newName", "", "", ""));
+        ParseResult actual = parser.getActionAndArguments("update 1 /taskType todo /taskName newName");
+        assertEquals(CommandType.UPDATE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_updateDeadlineByProvided_updateArgsParsed() {
+        ArrayList<String> expectedArgs = new ArrayList<>(
+                List.of("2", "deadline", "", "06/06/2026", "", ""));
+        ParseResult actual = parser.getActionAndArguments("update 2 /taskType deadline /by 06/06/2026");
+        assertEquals(CommandType.UPDATE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_updateEventFromToProvided_updateArgsParsed() {
+        ArrayList<String> expectedArgs = new ArrayList<>(
+                List.of("3", "event", "", "", "02/02/2026 14:00", "02/02/2026 16:00"));
+        ParseResult actual = parser.getActionAndArguments(
+                "update 3 /taskType event /from 02/02/2026 14:00 /to 02/02/2026 16:00");
+        assertEquals(CommandType.UPDATE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_updateNoArgsProvided_updateReturnedWithAllEmpty() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("", "", "", "", "", ""));
+        ParseResult actual = parser.getActionAndArguments("update");
+        assertEquals(CommandType.UPDATE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_updateOnlyTaskNoProvided_updateReturnedWithEmptyFields() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("1", "", "", "", "", ""));
+        ParseResult actual = parser.getActionAndArguments("update 1");
+        assertEquals(CommandType.UPDATE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
+    }
+
+    @Test
+    public void getActionAndArguments_updateFlagsMissingValues_updateReturnedWithEmptyValues() {
+        ArrayList<String> expectedArgs = new ArrayList<>(List.of("1", "", "", "", "", ""));
+        ParseResult actual = parser.getActionAndArguments("update 1 /taskType /taskName /by /from /to");
+        assertEquals(CommandType.UPDATE, actual.getCommandType());
+        assertEquals(expectedArgs, actual.getArgs());
     }
 }
